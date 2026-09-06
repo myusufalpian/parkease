55e3fe744269599322866d5dcd8be39a7ebf7a96
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2026-09-06 13:51:47 +0700

    initial commit

---
+++ src/test/java/id/xyz/parkease/ParkeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package id.xyz.parkease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
